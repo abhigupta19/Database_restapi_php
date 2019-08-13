b2697dbdec868fb8672a71f64f3d34dc76118724
Abhinav gupta <42437810+abhigupta19@users.noreply.github.com>
2019-08-13 23:11:23 +0530

    Update newresigter.php

--- Android/v1/newresigter.php
+++ Android/v1/newresigter.php
@@ -1,1 +1,33 @@
 
+<?php
+require_once '../include/Dboperation.php';
+$response=array();
+ if ($_SERVER['REQUEST_METHOD']=='POST')
+  {
+ 	if(
+ 		isset($_POST['username'])and isset($_POST['password']) and isset($_POST['email']))
+ 	{
+ 		$db=new Dboperation();
+ 		if($db->createUser($_POST['username'],$_POST['password'],$_POST['email']))
+ 		{
+ 			$response['error']='false';
+ 			$response['message']='succucs full regiterd';
+ 		}
+ 		else
+ 		{
+ 			$response['error']='true';
+ 			$response['message']='succucs full  not regiterd';
+ 		}
+ 	}
+ 	else{
+ 		$response['error']='true';
+ 			$response['message']='succucs missing in input';
+ 		}
+ 	}
+ 	else{
+ 		$response['error']='true';
+ 			$response['message']='succucs   not regiterd because method not right';
+ 		}
+ 	
+ 
+ echo json_encode($response);
